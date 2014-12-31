d39a898f3b71a94cf9037f6337c4555e9be4bd99
Lei Xia <ritchie_xl@hotmail.com>
2014-12-31 10:53:37 -0500

    rebuild cleaning.jar

--- src/vforce/lei/cleaning.java
+++ src/vforce/lei/cleaning.java
@@ -37,32 +37,32 @@ public class cleaning extends Configured implements Tool{
             String type = linkedHashMap.get("type").toString();
             if(type.equals("Account") &&
                     linkedHashMap.get("subAction").toString().equals("parentalControls")){
-                outputValue = "x:" + linkedHashMap.get("new").toString()+ ","+timeStamp;
+                outputValue = "x:" + linkedHashMap.get("new").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey),new Text(outputValue));
             }else if(type.equals("Account")){
-                outputValue = "c:"+linkedHashMap.get("subAction").toString()+ ","+timeStamp;
+                outputValue = "c:"+linkedHashMap.get("subAction").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey),new Text(outputValue));
             }else if(type.equals("AddToQueue")){
-                outputValue = "a:"+linkedHashMap.get("itemId").toString()+ ","+timeStamp;
+                outputValue = "a:"+linkedHashMap.get("itemId").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey),new Text(outputValue));
             }else  if(type.equals("Home")){
-                outputValue = "P:," + linkedHashMap.get("popular").toString()+ ","+timeStamp;
+                outputValue = "P:," + linkedHashMap.get("popular").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey),new Text(outputValue));
-                outputValue = "R:," + linkedHashMap.get("recommended").toString()+ ","+timeStamp;
+                outputValue = "R:," + linkedHashMap.get("recommended").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey),new Text(outputValue));
-                outputValue = "r:," + linkedHashMap.get("recent").toString()+ ","+timeStamp;
+                outputValue = "r:," + linkedHashMap.get("recent").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey),new Text(outputValue));
             }else  if(type.equals("Hover")){
-                outputValue = "h:"+linkedHashMap.get("itemId").toString()+ ","+timeStamp;
+                outputValue = "h:"+linkedHashMap.get("itemId").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey),new Text(outputValue));
             }else if(type.equals("ItemPage")){
-                outputValue = "i:"+linkedHashMap.get("itemId").toString()+ ","+timeStamp;
+                outputValue = "i:"+linkedHashMap.get("itemId").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey),new Text(outputValue));
             }else if(type.equals("Login")){
-                outputValue = "L:"+ ","+timeStamp;
+                outputValue = "L:"+ ":"+timeStamp;
                 output.collect(new Text(outputKey), new Text(outputValue));
             }else  if(type.equals("Logout")){
-                outputValue = "l:"+ ","+timeStamp;
+                outputValue = "l:"+ ":"+timeStamp;
                 output.collect(new Text(outputKey), new Text(outputValue));
             }else if(type.equals("Play") ||
                     type.equals("Pause") ||
@@ -70,35 +70,31 @@ public class cleaning extends Configured implements Tool{
                     type.equals("Stop") ||
                     type.equals("Advance") ||
                     type.equals("Resume")){
-                if(linkedHashMap.containsKey("payload") &&
-                        linkedHashMap.get("payload").getClass().equals(java.util.LinkedHashMap.class)){
-
-                    System.out.println("p:"+linkedHashMap.get("marker").toString() + "," +
-                            linkedHashMap.get("itemId").toString()+ ","+timeStamp);
+                if(linkedHashMap.containsKey("payload") && linkedHashMap.get("payload").toString().length() > 0){
                     outputValue = "p:"+linkedHashMap.get("marker").toString() + "," +
-                            linkedHashMap.get("itemId").toString()+ ","+timeStamp;
+                            linkedHashMap.get("itemId").toString()+ ":"+timeStamp;
                     output.collect(new Text(outputKey), new Text(outputValue));
                 }
             }else if(type.equals("Queue")){
-                outputValue = "q:"+ ","+timeStamp;
+                outputValue = "q:"+ ":"+timeStamp;
                 output.collect(new Text(outputKey), new Text(outputValue));
             }else if(type.equals("Rate")){
                 outputValue = "t:"+linkedHashMap.get("itemId").toString() + "," +
-                        linkedHashMap.get("rating").toString()+ ","+timeStamp;
+                        linkedHashMap.get("rating").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey), new Text(outputValue));
             }else if(type.equals("Recommendation")){
-                outputValue = "C:"+linkedHashMap.get("recs").toString()+ ","+timeStamp;
+                outputValue = "C:"+linkedHashMap.get("recs").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey), new Text(outputValue));
             }else if(type.equals("Search")){
-                outputValue = "S:"+linkedHashMap.get("results").toString()+ ","+timeStamp;
+                outputValue = "S:"+linkedHashMap.get("results").toString()+ ":"+timeStamp;
                 output.collect(new Text(outputKey), new Text(outputValue));
             }else  if(type.equals("VerifyPassword")){
-                outputValue = "v:"+ ","+timeStamp;
+                outputValue = "v:"+ timeStamp;
                 output.collect(new Text(outputKey), new Text(outputValue));
             }else if(type.equals("WriteReview")){
                 outputValue = "w:"+linkedHashMap.get("itemId").toString() + "," +
                         linkedHashMap.get("rating").toString() + "," +
-                        linkedHashMap.get("length").toString() + ","+timeStamp;
+                        linkedHashMap.get("length").toString() + ":"+timeStamp;
                 output.collect(new Text(outputKey), new Text(outputValue));
 
             }
